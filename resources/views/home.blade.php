1c0236dadaeb4587c83b5a508dd304d95b33e59c
Ahmed Assem <ahmed.asem@code95.com>
2025-03-06 15:00:39 +0200

    [add] logos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -150,7 +150,7 @@
                         <div class="row d-flex flex-wrap p-5">
                           @foreach ($partners as $i=> $partner)
                           @if ($i>3)
-                            <div class="col-6 col-md-3 p-5"><img src="{{$partner->logo}}"  class="d-block w-100"></div>
+                            <div class="col-6 col-md-3 p-5"><img src="{{ asset('storage/' . $partner->logo) }}"  class="d-block w-100"></div>
                           @endif
                           @endforeach
                         </div>
@@ -159,7 +159,7 @@
                         <div class="row d-flex flex-wrap p-5">
                           @foreach ($partners as $i=> $partner)
                           @if ($i<4)
-                            <div class="col-6 col-md-3 p-5"><img src="{{$partner->logo}}"  class="d-block w-100"></div>
+                            <div class="col-6 col-md-3 p-5"><img src="{{ asset('storage/' . $partner->logo) }}"  class="d-block w-100"></div>
                           @endif
                           @endforeach
                           
@@ -190,7 +190,7 @@
           <h1 class="col-md-12 text-center" style="margin-bottom: 25px;">{{ __('messages.success_stories') }}</h1>
           @foreach ($successStories as $i => $story)
           <div class="col-lg-6 col-md-6 content-item" style="{{ $i % 2 == 0 && app()->getLocale() == 'ar' ? 'border-left: 1px solid #d4e9e6;' : '' }} {{ $i % 2 == 0 && app()->getLocale() == 'en' ? 'border-right: 1px solid #d4e9e6;' : '' }} " data-aos="fade-in" data-aos-delay="100">
-            <span><img src="{{$story->logo}}" style="margin: auto; display: block;"></span>
+            <span><img src="{{ asset('storage/' . $story->logo) }}" style="margin: auto; display: block;"></span>
             <h4>{{ $locale == 'ar' ? $story->name_ar : $story->name_en }}</h4>
             <p>{{ $locale == 'ar' ? $story->description_ar : $story->description_en }}</p>
           </div>
